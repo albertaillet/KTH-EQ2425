a1eda431eaf87f203337b782dbbfbf8b0dc72b76
simon <bonato@kth.se>
2022-09-21 11:41:39 +0200

    functions until tree construction

--- project2/exploration.py
+++ project2/exploration.py
@@ -1,23 +1,24 @@
-# %% Project imports
-
+# %% Project imports and functions
 import os
+from random import random
 import cv2 
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
-
+import scipy.cluster.hierarchy as shc
+from sklearn.cluster import KMeans
 from tqdm import tqdm
+import re
+
+
 
 # for typing
 from numpy import ndarray
-from typing import Union
+from typing import List, Union
 detector_type = Union[cv2.xfeatures2d_SIFT, cv2.xfeatures2d_SURF]
 
 DATA_FOLDER = 'data2'
 OUT_FOLDER = 'output2'
 PLAY_FOLDER = 'playground2'
-
-#  %% Load data
 
 def load_data(img_folder: str):
     '''Loads all the images from img folder and stores them into 2 separate dictionaries (client and server)'''
@@ -41,6 +42,10 @@ def save_img(img: ndarray, name: str, kp: list=None, folder: str=PLAY_FOLDER):
     # save image using cv
     cv2.imwrite(f'{folder}/{name}.jpg', img)
 
+def get_obj_number(img_name: str, start: str = 'obj', end: str = '_') -> int:
+    result = re.search(f'{start}(.*){end}', img_name)
+    return int(result.group(1))
+
 def plot(x, y, xlabel, ylabel, title, xlim=None, filename=None):
     plt.plot(x, y, 'x-')
     plt.title(title)
@@ -60,6 +65,26 @@ def plot(x, y, xlabel, ylabel, title, xlim=None, filename=None):
 def detect_keypoints(img: ndarray, detector: detector_type) -> list:
     return detector.detect(img, None)
 
+def hi_kmeans(data: list, b: int, depth: int):
+    '''
+    Builds a hierarchical tree using the given keypoints.
+    :param b: number of cluster for each iteration Tree building algorithm.
+    :param depth: number of iterations to build the Tree.
+    expl: https://github.com/epignatelli/scalable-recognition-with-a-vocabulary-tree/blob/master/_test_Scalable%20Recognition%20with%20a%20Vocabulary%20Tree%20copy.ipynb
+    '''
+
+    Kmeans = KMeans(n_clusters=b, random_state=0)
+    tree_struc = {}
+    for t in range(depth):
+        new_labels = KMeans.fit_predict(X=data)
+
+def get_descr_list(server_desc_dict):
+    l = list()
+    for key in server_desc_dict.keys():
+        for desc in server_desc_dict[key]:
+            l.append(desc)
+    return l
+
 # %% Data loading
 
 imgs = load_data('data2')
@@ -69,29 +94,37 @@ client_imgs = imgs['client']
 server_imgs = imgs['server']
 
 # %%
-
 # Create SIFT detector
 # have to choose the parameters!
 edgeT = 10
 contrastT = 0.125
 sift = cv2.xfeatures2d.SIFT_create(edgeThreshold=edgeT, contrastThreshold=contrastT)
 
-# dicts with structure {'obj_number': list with SIFT keypoints}
 client_kp = {}
+client_desc = {}
 server_kp = {}
-
+server_desc = {}
+# %%
 # detect keypoints in client images 
-for idx, name in tqdm(enumerate(client_imgs.keys())):
-    obj_number = idx + 1
-    kp = detect_keypoints(client_imgs[name], sift)
+for img_name in tqdm(client_imgs.keys()):
+    obj_number = get_obj_number(img_name)
+    kp, desc = sift.detectAndCompute(client_imgs[img_name], None)
     client_kp[obj_number] = kp
+    client_desc[obj_number] = desc
 
-for idx, name in tqdm(enumerate(server_imgs.keys())):
-    obj_number = int(idx / 3) + 1
+# %%
+# store keypoints and descriptors in a dictionary based on object number
+for img_name in tqdm(server_imgs.keys()):
+    obj_number = get_obj_number(img_name)
     if obj_number not in server_kp.keys():
         server_kp[obj_number] = []
-    kp = detect_keypoints(server_imgs[name], sift)
+        server_desc[obj_number] = []
+    
+    kp, desc = sift.detectAndCompute(server_imgs[img_name], None)
     server_kp[obj_number] += kp
+    for d in desc:
+        server_desc[obj_number].append(d)
+
 # %%
 # compute the avg number of features per object
 avg_features_client = 0
@@ -102,6 +135,5 @@ for obj in client_kp.keys():
 
 print(f'The average number of features is:\nserver images: {int(avg_features_server / 50)}\nclient images: {int(avg_features_client / 50)}')
 
-
-
 # %%
+server_desc_list = get_descr_list(server_desc)
