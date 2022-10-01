1935b3d68e7f660c503c11131bb709b1f2782480
Albert Sund Aillet <albert@sundaillet.com>
2022-10-02 00:37:52 +0200

    added 3d plot

--- project2/plotting.py
+++ project2/plotting.py
@@ -1,9 +1,11 @@
 # %% Imports
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 FOLDER = 'output2'
 # %%
 df = pd.read_json(f'{FOLDER}/results.json')
+df = df.drop(columns=['top_k_recall'])
 # %%
 df_depth = df[(df['b'] == 4) & (df['n_components'] == 1)].sort_values('depth')
 depths = df_depth['depth']
@@ -35,4 +37,24 @@ plt.legend(loc='lower right')
 plt.title('Recall depending on b for depth=5')
 plt.grid()
 plt.savefig(f'{FOLDER}/b_vs_recall.png')
+# %% plot 3d plane with b, depth, recall
+
+df_3d = df[(df['n_components'] == 1) & (df['perc']==1)].drop_duplicates().sort_values(['b', 'depth'])
+n_bs = len(df_3d['b'].unique())
+n_depths = len(df_3d['depth'].unique())
+depths = df_3d['depth'].values.reshape(n_bs, n_depths)
+bs = df_3d['b'].values.reshape(n_bs, n_depths)
+top_1_recall = df_3d['top_1_recall'].values.reshape(n_bs, n_depths)
+top_5_recall = df_3d['top_5_recall'].values.reshape(n_bs, n_depths)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot_surface(bs, depths, top_1_recall, label='Top-1 Recall', alpha=0.5)
+ax.plot_surface(bs, depths, top_5_recall, label='Top-5 Recall', alpha=0.5)
+ax.set_xlabel('b')
+ax.set_ylabel('depth')
+ax.set_zlabel('Recall')
+plt.title('Recall depending on b and depth')
+
 # %%
